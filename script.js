f7965fe48714ffd554779f5a825d9117824e7761
Sleet <80127288+Sleet827@users.noreply.github.com>
2024-06-20 23:51:37 +0400

    anniversary 3.55

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 window.onload = function() {
     var audio = document.getElementById('romanticAudio');
     audio.volume = 0.5;
+    
+    audio.addEventListener('canplaythrough', function() {
+        console.log("Audio can play through.");
+    });
+
+    audio.addEventListener('error', function(e) {
+        console.error("Error loading audio: ", e);
+    });
 };
