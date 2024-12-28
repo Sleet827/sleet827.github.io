26e2e3086b19d84b268df7b7005f4b5a67c9937e
Sleet <80127288+Sleet827@users.noreply.github.com>
2024-12-28 13:49:05 +0400

    foo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.getElementById('changeColorBtn').addEventListener('click', function() {
     const colors = ['#ff6347', '#4682b4', '#32cd32', '#ff69b4', '#ffd700'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
+    createSnowflake();
 });
 
 // Snowflake generation
