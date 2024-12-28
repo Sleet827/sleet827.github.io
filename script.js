14d02cd8cbb7dc3b18617e9d0e75d0623819af2a
Sleet <80127288+Sleet827@users.noreply.github.com>
2024-12-28 13:54:09 +0400

    bar

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-document.getElementById('changeColorBtn').addEventListener('click', function() {
-    const colors = ['#ff6347', '#4682b4', '#32cd32', '#ff69b4', '#ffd700'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
-    createSnowflake();
-});
+document.getElementById('snowflakeBtn').addEventListener('click', createSnowflake());
 
 // Snowflake generation
 function createSnowflake() {
     const snowflake = document.createElement('div');
     snowflake.classList.add('snowflake');
-    const size = Math.random() * 10 + 5; // Random size between 5px and 15px
+    const size = Math.random() * 10 + 5;
     snowflake.style.width = `${size}px`;
     snowflake.style.height = `${size}px`;
-    snowflake.style.left = Math.random() * 100 + 'vw'; // Random horizontal position
-    snowflake.style.animationDuration = Math.random() * 3 + 2 + 's'; // Random fall duration
+    snowflake.style.left = Math.random() * 100 + 'vw';
+    snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
     document.querySelector('.snow').appendChild(snowflake);
 
     // Remove snowflake after it falls
@@ -21,5 +16,3 @@ function createSnowflake() {
         snowflake.remove();
     });
 }
-
-//
